a19d91e623bbe670aacbe46e4023039843f19a9d
Oleg <o.mullakaev@gmail.com>
2018-04-25 23:25:59 +0300

    вывод сообщения

--- app/js/ink-calc.js
+++ app/js/ink-calc.js
@@ -5,13 +5,17 @@ $(() => {
     const printArea = $('#print-area');
     const k = 0.3;
     const mmToM = 1000;
+    let answerBlock = $('.answer-block__row');
 
     const calculate = (pmNumber, widthNumber, cellNumber, printAreaNumber) => {
-        let answer = pmNumber * (widthNumber / mmToM) * k * cellNumber * printAreaNumber / 1000;
+        let answer = pmNumber * (widthNumber / mmToM) * k * cellNumber * (printAreaNumber / 100)/ 1000;
         return answer.toFixed(3)
     };
 
     const print = (cal) => {
+        let answerCal = cal + ' кг';
+        let answerHTML = '<p>' + answerCal + '</p>';
+        answerBlock.append(answerHTML);
         console.log(cal);
         //todo: нужно сделать вывод информации и проверить еще раз вычисления
     };
